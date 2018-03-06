9c143c69cf480d4f7748a0072f64cdeff0c25f6f
zhangdachuan <471841715@qq.com>
2018-03-06 20:51:52 +0800

    change

--- __init__.py
+++ __init__.py
@@ -1,5 +1,27 @@
 #encoding: utf-8
+
 from flask import *
+import time
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+PRODUCT_FILENAME = 'movies.txt'
+#常量，存的是电影信息的文件名
+
+def read_file(filename):
+    '''
+    用来读取文件内容，返回一个字典
+    :param filename: 文件名
+    :return: 文件N内容的字典
+    :
+    '''
+    with open(filename,'a+') as fr:
+        fr.seek(0)    #2从末尾开始
+        content = fr.read()
+        print content
+        if len(content):#这里判断文件内容是否为空的，如果不为0的话就为是真
+           return eval(content)
+        return []
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -26,6 +48,13 @@ def submit_form():
 def get_form():
     return render_template('form.html')
 
+@app.route('/movie_show', methods=['get'])
+def get_movie_show():
+
+    movies = read_file(PRODUCT_FILENAME)  # 获取电影信息
+    movie = movies[0]
+
+    return render_template('movie_show.html',**movie)
 
 
 
