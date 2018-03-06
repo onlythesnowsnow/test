59597d9c2db798737a2f29f1aa6d012c2e0bcd30
zhangdachuan <471841715@qq.com>
2018-03-06 22:38:52 +0800

    change4

--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@ PRODUCT_FILENAME = 'movies.txt'
 USER_FILENAME = 'user.txt'
 #常量，存的是存储用户信息的文件名
 
+
+#文件操作
 def read_file(filename):
     '''
     用来读取文件内容，返回一个字典
@@ -24,6 +26,18 @@ def read_file(filename):
         if len(content):#这里判断文件内容是否为空的，如果不为0的话就为是真
            return eval(content)
         return []
+
+def write_file(filename,content):
+    '''
+    用来读取文件内容，返回一个字典
+    :param filename: 文件名
+    :return: 文件N内容的字典
+    '''
+    with open(filename,'a+') as fw:
+        fw.seek(0)
+        fw.truncate()
+        fw.write(str(content))
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -58,6 +72,9 @@ def get_movie_show():
 
     return render_template('movie_show.html',**movie)
 
+
+
+#用户登录相关函数
 @app.route('/login', methods=['get'])
 def get_login():
     return render_template('login.html')
@@ -83,6 +100,37 @@ def submit_login():
     else:
         logo['logo'] = u'用户名或密码不能为空'
         return render_template('login_result.html', **logo)
+
+#用户注册相关函数
+@app.route('/register', methods=['get'])
+def get_register():
+    return render_template('register.html')
+
+@app.route('/register', methods=['post'])
+def submit_register():
+    users = read_file(USER_FILENAME)
+    user = {}
+    logo = {}
+    n = 0
+    user['username'] = request.form['username']
+    user['password'] = request.form['password']
+    if user['username'] != '' and user['password'] != '':
+        for u in users:
+            n = n + 1
+            if user['username'] == u['username'] :
+                #注册失败
+                logo['logo'] = u'用户名已经存在'
+                return render_template('register_result.html', **logo)
+                break
+            if (n == len(users)):
+                logo['logo'] = u'注册成功'
+                users.append(user)
+                write_file(USER_FILENAME, users)
+                return render_template('register_result.html', **logo)
+
+    else:
+        logo['logo'] = u'用户名或密码不能为空'
+        return render_template('register_result.html', **logo)
 
 
 if __name__ == '__main__':
