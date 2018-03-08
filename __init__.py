65ce7fa7d92d446e01c56d9e82e74ac64ac1c1af
zhangdachuan <471841715@qq.com>
2018-03-08 16:05:38 +0800

    啧

--- __init__.py
+++ __init__.py
@@ -12,6 +12,7 @@ USER_FILENAME = 'user.txt'
 #常量，存的是存储用户信息的文件名
 LOG_FILENAME = 'movie.log'
 #常量，存的是日志文件名
+admin = 'admin'
 
 
 #文件操作
@@ -55,6 +56,30 @@ def write_log(username,operation):
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+def is_time(s):
+    '''
+    这个函数的作用是用来判断价格是否合法，
+    :param s:
+    :return:
+    '''
+
+    s = str(s)
+    if s.count('.')==1:#判断小数点个数
+        sl = s.split('.')#按照小数点进行分割
+        left = sl[0]#小数点前面的
+        right = sl[1]#小数点后面的
+        if left.startswith('-') and left.count('-')==1 and right.isdigit():
+            lleft = left.split('-')[1]#按照-分割，然后取负号后面的数字
+            if lleft.isdigit():
+                return False
+        elif left.isdigit() and right.isdigit():
+            #判断是否为正小数
+            return True
+    elif s.isdigit():
+        s = int(s)
+        if s!=0:
+            return True
+    return False
 
 #主页相关函数
 @app.route('/')
@@ -68,6 +93,7 @@ def index():
 
 
 #测试相关函数
+'''
 @app.route('/form', methods=['post'])
 def submit_form():
 
@@ -80,14 +106,90 @@ def submit_form():
 @app.route('/form', methods=['get'])
 def get_form():
     return render_template('form.html')
+'''
 
 
 
-#电影相关函数
+#查看全部电影函数
 @app.route('/base', methods=['get'])
 def get_movie_all():
 
     return render_template('movie_all.html')
+
+#电影发布函数
+@app.route('/index', methods=['get'])
+def get_movie_publish():
+
+    return render_template('movie_publish.html')
+
+@app.route('/movie_publish', methods=['post'])
+def post_movie_publish():
+
+    movie = {}
+    logo = {}
+    movies = read_file(MOVIES_FILENAME)
+    y = 0
+    movie['title'] = request.form['title']
+    movie['year'] = request.form['year']
+    movie['country'] = request.form['country']
+    movie['category'] = request.form['category']
+    movie['duration'] = request.form['duration']
+    movie['director'] = request.form['director']
+    movie['profile'] = request.form['profile']
+    movie['screenshot'] = ''
+    movie['cover'] = ''
+    movie['down_load'] = ''
+    movie['douban_rating'] = ''
+    if movie['title'] != '':
+        for x in movies:
+            if movie['title'] == x['title']:
+                logo['logo'] = u'输入的电影已经存在'
+                return render_template('movie_result.html',**logo)
+            if not is_time(movie['year']):
+                logo['logo'] = u'输入的电影年代不合法'
+                return render_template('movie_result.html',**logo)
+            y = y + 1
+            if (y == len(movies)):
+                write_log(admin,'添加了电影信息 电影名【%s】 '%(movie['title']))
+                logo['logo'] = u'发布电影成功'
+                movies.insert( 0, movie)
+                write_file(MOVIES_FILENAME,movies)
+                logo['logo'] = u'电影发布成功'
+
+                return render_template('movie_result.html',**logo)
+    else:
+        logo['logo'] = u'电影名称不能为空'
+        return render_template('movie_result.html,**logo')
+
+#电影删除函数
+@app.route('/movie_delete', methods=['get'])
+def get_movie_delete():
+
+    return render_template('movie_delete.html')
+
+@app.route('/movie_delete', methods=['post'])
+def post_movie_delete():
+
+    movies = read_file(MOVIES_FILENAME)
+    movie = {}
+    logo = {}
+    movie['title'] = request.form['title']
+    y = 0
+    if movie['title'] !='':
+        for x in movies:
+            if movie['title'] == x['title']:
+                del movies[y]
+                write_log(admin,'删除了电影信息 电影名【%s】' %movie['title'])
+                write_file(MOVIES_FILENAME,movies)
+                logo['logo'] = u'删除电影成功'
+                return render_template('movie_result.html', **logo)
+            if  (y == len(movies)):
+                logo['logo'] = u'删除的电影不存在'
+                return render_template('movie_result.html', **logo)
+            y = y + 1
+    else :
+        logo['logo'] = u'删除的电影名称不能为空'
+        return render_template('movie_result.html', **logo)
 
 @app.route('/movie_list',methods = ['post'])
 def get_movie_show_id():
@@ -288,10 +390,10 @@ def submit_login():
                 return render_template('index.html', **user)
             if (n == len(users)):
                 logo['logo'] = u'用户名或密码错误'
-                return render_template('login_result.html', **logo)
+                return render_template('movie_result.html', **logo)
     else:
         logo['logo'] = u'用户名或密码不能为空'
-        return render_template('login_result.html', **logo)
+        return render_template('movie_result.html', **logo)
 
 #用户注册相关函数
 @app.route('/register', methods=['get'])
@@ -312,18 +414,18 @@ def submit_register():
             if user['username'] == u['username'] :
                 #注册失败
                 logo['logo'] = u'用户名已经存在'
-                return render_template('register_result.html', **logo)
+                return render_template('movie_result.html', **logo)
                 break
             if (n == len(users)):
                 write_log(user['username'], '注册成功！')
                 logo['logo'] = u'注册成功'
                 users.append(user)
                 write_file(USER_FILENAME, users)
-                return render_template('register_result.html', **logo)
+                return render_template('movie_result.html', **logo)
 
     else:
         logo['logo'] = u'用户名或密码不能为空'
-        return render_template('register_result.html', **logo)
+        return render_template('movie_result.html', **logo)
 
 
 if __name__ == '__main__':
